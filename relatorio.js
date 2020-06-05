b9e796569b6caa3c89b37f3f1e6f0b5538206347
Luís Henrique Pereira <lh.p@hotmail.com>
2020-06-05 18:32:49 -0300

    Criando Servidor

---
+++ relatorio.js
@@ -0,0 +1,12 @@
+//***npm start => Liga o servidor
+
+//***Instalando template no npm
+//npm install nunjucks
+
+
+
+
+
+
+//OBS: Configurações de preferências do JSON
+//Ctrl+Shift+p => >Preferences: Open Settings(JSON)
---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const server = express()
+
+//***Configurar pasta pública
+//Arquivos estáticos
+server.use(express.static("public"))
+
+//***Utilizando template engine => (Permite html dinâmico)
+//***const nunjucks = require("nunjucks") => Requisitando o nunjucks instalado no npm
+const nunjucks = require("nunjucks")
+
+//Configuração nunjucks("Pastas com html", "{propriedades}")
+nunjucks.configure("src/views", {
+    express: server, //Ligando o nunjucks ao servidor
+    noCache: true //Sem Cache durante o desenvolvimento
+})
+
+
+//***Renderizar as paginas html
+//O (.render) do servidor passa pelo nunjucks onde estão os arq. html
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+
+//***Ligar o servidor
+server.listen(3000)
